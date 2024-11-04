176bb618bd55bf26fb7e4cae8bff9f022c921aba
giv3x <giv3xconnect@gmail.com>
2024-11-05 00:38:46 +0700

    finish

--- tutor/snippets/views.py
+++ tutor/snippets/views.py
@@ -3,28 +3,41 @@ from .models import Snippet
 from .serializers import SnippetSerializer,UserSerializer
 from django.contrib.auth.models import User
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework import viewsets
+from rest_framework.decorators import action
 
-class SnippetList(generics.ListCreateAPIView):
+
+class SnippetViewSet(viewsets.ModelViewSet):
+
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
-   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                         IsOwnerOrReadOnly]
+
+   @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+   def highlight(self, request, *args, **kwargs):
+       snippet = self.get_object()
+       return Response(snippet.highlighted)
+
    def perform_create(self, serializer):
        serializer.save(owner=self.request.user)
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-   queryset = Snippet.objects.all()
-   serializer_class = SnippetSerializer
-   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-   permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+@api_view(['GET'])
+def api_root(request, format=None):
+   return Response({
+       'users': reverse('user-list', request=request, format=format),
+       'snippets': reverse('snippet-list', request=request, format=format)
+   })
 
 
-class UserList(generics.ListAPIView):
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+
    queryset = User.objects.all()
    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-   queryset = User.objects.all()
-   serializer_class = UserSerializer
-
